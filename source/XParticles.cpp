b0c0a9d5fbe3cb0a985467a335944ec045dc030b
zhangliang <zhangliang>
2019-09-04 14:42:14 +0800

    feat: merge repeat code and remove unused code

--- source/XParticles.cpp
+++ source/XParticles.cpp
@@ -1,40 +1,118 @@
-//
-// Created by 林炳河 on 2019-09-03.
-//
-
 #include "XParticles.hpp"
-#include "bgfx_utils.h"
+#include "XImageUtils.hpp"
+#include <bgfx/bgfx.h>
+#include <bgfx/embedded_shader.h>
+#include "ps/vs_particle.bin.h"
+#include "ps/fs_particle.bin.h"
 
 NS_X_IMAGE_BEGIN
+bgfx::VertexDecl PosColorTexCoord0Vertex::ms_decl;
+
+static const bgfx::EmbeddedShader s_embeddedShaders[] =
+        {
+                BGFX_EMBEDDED_SHADER(vs_particle),
+                BGFX_EMBEDDED_SHADER(fs_particle),
+
+                BGFX_EMBEDDED_SHADER_END()
+        };
+
+static int32_t particleSortFn(const void *_lhs, const void *_rhs) {
+    const ParticleSort &lhs = *(const ParticleSort *) _lhs;
+    const ParticleSort &rhs = *(const ParticleSort *) _rhs;
+    return lhs.dist > rhs.dist ? -1 : 1;
+}
+
 
 XParticles::XParticles() {
-    mParticleSystem = new XParticleSystem();
+    mViewMatrix = nullptr;
+    mAllocator = nullptr;
+    mEmitterAlloc = nullptr;
+    mEmitter = nullptr;
+
+    mTexColor = {bgfx::kInvalidHandle};
+    mTexture = {bgfx::kInvalidHandle};
+    mParticleProgram = {bgfx::kInvalidHandle};
 }
 
 XParticles::~XParticles() {
-    mParticleSystem->shutdown();
+    bgfx::destroy(mParticleProgram);
+    bgfx::destroy(mTexture);
+    bgfx::destroy(mTexColor);
+
+    bx::destroyHandleAlloc(mAllocator, mEmitterAlloc);
+    BX_FREE(mAllocator, mEmitter);
+
+    mAllocator = NULL;
 }
 
-void XParticles::init(uint16_t maxEmitters, bx::AllocatorI* allocator) {
-    mParticleSystem->init(maxEmitters, allocator);
+void XParticles::init(uint16_t maxEmitters, bx::AllocatorI *allocator) {
+    mAllocator = allocator;
+
+    if (nullptr == allocator) {
+        static bx::DefaultAllocator allocator;
+        mAllocator = &allocator;
+    }
+
+    mEmitterAlloc = bx::createHandleAlloc(mAllocator, maxEmitters);
+    mEmitter = (Emitter *) BX_ALLOC(mAllocator, sizeof(Emitter) * maxEmitters);
+
+    PosColorTexCoord0Vertex::init();
+
+    mNum = 0;
+
+    mTexColor = bgfx::createUniform("s_texColor", bgfx::UniformType::Sampler);
+    mTexture = bgfx::createTexture2D(
+            SPRITE_TEXTURE_SIZE, SPRITE_TEXTURE_SIZE, false, 1, bgfx::TextureFormat::BGRA8);
+
+    bgfx::RendererType::Enum type = bgfx::getRendererType();
+    mParticleProgram = bgfx::createProgram(
+            bgfx::createEmbeddedShader(s_embeddedShaders, type, "vs_particle"),
+            bgfx::createEmbeddedShader(s_embeddedShaders, type, "fs_particle"), true);
 }
 
 EmitterHandle XParticles::createEmitter(EmitterShape::Enum shape, EmitterDirection::Enum direction,
-                                     uint32_t maxParticles) {
-    return mParticleSystem->createEmitter(shape, direction, maxParticles);
+                                        uint32_t maxParticles) {
+    EmitterHandle handle = {mEmitterAlloc->alloc()};
+
+    if (UINT16_MAX != handle.idx) {
+        mEmitter[handle.idx].create(shape, direction, maxParticles, mAllocator);
+    }
+
+    return handle;
 }
 
 EmitterSpriteHandle XParticles::createSprite(std::string filePath, bgfx::TextureFormat::Enum dstFormat) {
     bimg::ImageContainer *image = imageLoad(filePath.data(), dstFormat);
-    return mParticleSystem->createSprite(image->m_width, image->m_height, image->m_data);
+    EmitterSpriteHandle handle = mSprite.create(image->m_width, image->m_height);
+
+    if (isValid(handle)) {
+        const Pack2D &pack = mSprite.get(handle);
+        bgfx::updateTexture2D(
+                mTexture, 0, 0, pack.m_x, pack.m_y, pack.m_width, pack.m_height,
+                bgfx::copy(image->m_data, pack.m_width * pack.m_height * 4));
+    }
+    bimg::imageFree(image);
+    return handle;
+
 }
 
 void XParticles::destroyEmitter(EmitterHandle handle) {
-    mParticleSystem->destroyEmitter(handle);
+    BX_CHECK(mEmitterAlloc.isValid(handle.idx), "destroyEmitter handle %d is not valid.", handle.idx);
+
+    mEmitter[handle.idx].destroy(mAllocator);
+    mEmitterAlloc->free(handle.idx);
 }
 
 void XParticles::updateEmitter(EmitterHandle handle, EmitterUniforms *uniforms) {
-    mParticleSystem->updateEmitter(handle, uniforms);
+    BX_CHECK(mEmitterAlloc.isValid(handle.idx), "destroyEmitter handle %d is not valid.", handle.idx);
+
+    Emitter &emitter = mEmitter[handle.idx];
+
+    if (nullptr == uniforms) {
+        emitter.reset();
+    } else {
+        bx::memCopy(&emitter.m_uniforms, uniforms, sizeof(EmitterUniforms));
+    }
 }
 
 void XParticles::updateViewAndEye(float *mtxView, const bx::Vec3 &eye) {
@@ -43,11 +121,85 @@ void XParticles::updateViewAndEye(float *mtxView, const bx::Vec3 &eye) {
 }
 
 void XParticles::renderAtProgress(float progress, int index) {
-    mParticleSystem->update(progress);
-    mParticleSystem->render(index, mViewMatrix, mEyePos);
+    uint32_t numParticles = 0;
+    for (uint16_t ii = 0, num = mEmitterAlloc->getNumHandles(); ii < num; ++ii) {
+        const uint16_t idx = mEmitterAlloc->getHandleAt(ii);
+        Emitter &emitter = mEmitter[idx];
+        emitter.update(progress);
+        numParticles += emitter.m_num;
+    }
+
+    mNum = numParticles;
+
+    if (0 != mNum) {
+        bgfx::TransientVertexBuffer tvb;
+        bgfx::TransientIndexBuffer tib;
+
+        const uint32_t numVertices = bgfx::getAvailTransientVertexBuffer(mNum * 4,
+                                                                         PosColorTexCoord0Vertex::ms_decl);
+        const uint32_t numIndices = bgfx::getAvailTransientIndexBuffer(mNum * 6);
+        const uint32_t max = bx::uint32_min(numVertices / 4, numIndices / 6);
+        BX_WARN(mNum == max,
+                "Truncating transient buffer for particles to maximum available (requested %d, available %d).",
+                mNum, max);
+
+        if (0 < max) {
+            bgfx::allocTransientBuffers(&tvb, PosColorTexCoord0Vertex::ms_decl, max * 4, &tib, max * 6);
+            PosColorTexCoord0Vertex *vertices = (PosColorTexCoord0Vertex *) tvb.data;
+
+            ParticleSort *particleSort = (ParticleSort *) BX_ALLOC(mAllocator, max * sizeof(ParticleSort));
+
+            uint32_t pos = 0;
+            for (uint16_t ii = 0, numEmitters = mEmitterAlloc->getNumHandles(); ii < numEmitters; ++ii) {
+                const uint16_t idx = mEmitterAlloc->getHandleAt(ii);
+                Emitter &emitter = mEmitter[idx];
+
+                const Pack2D &pack = mSprite.get(emitter.m_uniforms.sprite);
+                const float invTextureSize = 1.0f / SPRITE_TEXTURE_SIZE;
+                const float uv[4] =
+                        {
+                                pack.m_x * invTextureSize,
+                                pack.m_y * invTextureSize,
+                                (pack.m_x + pack.m_width) * invTextureSize,
+                                (pack.m_y + pack.m_height) * invTextureSize,
+                        };
+
+                pos += emitter.render(uv, mViewMatrix, mEyePos, pos, max, particleSort, vertices);
+            }
+
+            qsort(particleSort, max, sizeof(ParticleSort), particleSortFn);
+
+            uint16_t *indices = (uint16_t *) tib.data;
+            for (uint32_t ii = 0; ii < max; ++ii) {
+                const ParticleSort &sort = particleSort[ii];
+                uint16_t *index = &indices[ii * 6];
+                uint16_t idx = (uint16_t) sort.idx;
+                index[0] = idx * 4 + 0;
+                index[1] = idx * 4 + 1;
+                index[2] = idx * 4 + 2;
+                index[3] = idx * 4 + 2;
+                index[4] = idx * 4 + 3;
+                index[5] = idx * 4 + 0;
+            }
+
+            BX_FREE(mAllocator, particleSort);
+
+            bgfx::setState(0
+                           | BGFX_STATE_WRITE_RGB
+                           | BGFX_STATE_WRITE_A
+                           | BGFX_STATE_DEPTH_TEST_LESS
+                           | BGFX_STATE_CULL_CW
+                           | BGFX_STATE_BLEND_NORMAL);
+            bgfx::setVertexBuffer(0, &tvb);
+            bgfx::setIndexBuffer(&tib);
+            bgfx::setTexture(0, mTexColor, mTexture);
+            bgfx::submit(index, mParticleProgram);
+        }
+    }
 }
 
 void XParticles::getAabb(EmitterHandle handle, Aabb &outAabb) {
-    mParticleSystem->getAabb(handle, outAabb);
+    BX_CHECK(mEmitterAlloc.isValid(handle.idx), "getAabb handle %d is not valid.", handle.idx);
+    outAabb = mEmitter[handle.idx].m_aabb;
 }
 NS_X_IMAGE_END
--- source/XParticles.hpp
+++ source/XParticles.hpp
@@ -6,37 +6,137 @@
 #define GPUIMAGE_X_XPARTICLES_HPP
 
 #include "XMacros.hpp"
-#include "XParticleSystem.hpp"
 #include "XImageInput.hpp"
 #include "XImageOutput.hpp"
+#include "PackRect.hpp"
+#include "bounds.h"
+#include "Emitter.h"
+#include <bx/allocator.h>
+#include <bx/easing.h>
+#include <bx/rng.h>
+#include <bx/handlealloc.h>
 
 NS_X_IMAGE_BEGIN
+
+#define SPRITE_TEXTURE_SIZE 1024
+template<uint16_t MaxHandlesT = 256, uint16_t TextureSizeT = 1024>
+struct SpriteT
+{
+    SpriteT() : rectPack2Dt(TextureSizeT, TextureSizeT) {}
+
+    EmitterSpriteHandle create(uint16_t width, uint16_t height) {
+        EmitterSpriteHandle handle = { bx::kInvalidHandle };
+
+        if (handleAllocT.getNumHandles() < handleAllocT.getMaxHandles() )
+        {
+            Pack2D pack;
+            if (rectPack2Dt.find(width, height, pack) )
+            {
+                handle.idx = handleAllocT.alloc();
+                pack2D[handle.idx] = pack;
+            }
+        }
+
+        return handle;
+    }
+
+    void destroy(EmitterSpriteHandle _sprite) {
+        const Pack2D& pack = pack2D[_sprite.idx];
+        rectPack2Dt.clear(pack);
+        handleAllocT.free(_sprite.idx);
+    }
+
+    const Pack2D& get(EmitterSpriteHandle _sprite) const {
+        return pack2D[_sprite.idx];
+    }
+
+    bx::HandleAllocT<MaxHandlesT> handleAllocT;
+    Pack2D                        pack2D[MaxHandlesT];
+    RectPack2DT<256>              rectPack2Dt;
+};
+
+template<typename Ty>
+inline bool isValid(Ty handle)
+{
+    return handle.idx != UINT16_MAX;
+}
+
 class XParticles: public XImageInput, public XImageOutput {
 public:
     XParticles();
 
     ~XParticles();
 
+    /**
+     * @brief init particle data, must call first
+     * @param maxEmitters max number of emitters
+     * @param allocator memory allocator
+     */
     void init(uint16_t maxEmitters = 64, bx::AllocatorI* allocator = NULL);
 
+    /**
+     * @brief create one emitter
+     * @param shape shape of emission
+     * @param direction direction of emission
+     * @param maxParticles max number of particle
+     * @return created emitter handle
+     */
     EmitterHandle createEmitter(EmitterShape::Enum shape, EmitterDirection::Enum direction, uint32_t maxParticles);
 
+    /**
+     * @brief create one sprite
+     * @param filePath file path of sprite
+     * @param dstFormat the format sprite need to loaded
+     * @return created sprite handle
+     */
     EmitterSpriteHandle createSprite(std::string filePath, bgfx::TextureFormat::Enum dstFormat);
 
+    /**
+     * @brief update selected handle
+     * @param handle selected handle
+     * @param uniforms updated parameter
+     */
     void updateEmitter(EmitterHandle handle, EmitterUniforms *uniforms = nullptr);
 
+    /**
+     * @brief update view matrix and eye position
+     * @param mtxView view matrix
+     * @param eye eye position
+     */
     void updateViewAndEye(float *mtxView, const bx::Vec3 &eye);
 
     void renderAtProgress(float progress, int index) override;
 
+    /**
+     * @brief destroy selected emitter
+     * @param handle selected emitter
+     */
     void destroyEmitter(EmitterHandle handle);
 
+    /**
+     * @brief get aabb of emission
+     * @param handle selected handle
+     * @param outAabb aabb value
+     */
     void getAabb(EmitterHandle handle, Aabb& outAabb);
 
 private:
-    XParticleSystem *mParticleSystem;
     float *mViewMatrix;
     bx::Vec3 mEyePos;
+
+    bx::AllocatorI* mAllocator;
+
+    bx::HandleAlloc* mEmitterAlloc;
+    Emitter* mEmitter;
+
+    typedef SpriteT<256, SPRITE_TEXTURE_SIZE> Sprite;
+    Sprite mSprite;
+
+    bgfx::UniformHandle mTexColor;
+    bgfx::TextureHandle mTexture;
+    bgfx::ProgramHandle mParticleProgram;
+
+    uint32_t mNum;
 };
 NS_X_IMAGE_END
 
