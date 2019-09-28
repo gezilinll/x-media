eb1b85ab21d2e316b495e3dafc25a4d56e70d7a1
linbinghe <linbinghe@gmail.com>
2019-09-28 21:28:21 +0800

    feat:简单添加XTransform作为变换控制类

---
+++ source/XTransform.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 林炳河 on 2019/9/28.
+//
+
+#include "XTransform.hpp"
---
+++ source/XTransform.hpp
@@ -0,0 +1,16 @@
+//
+// Created by 林炳河 on 2019/9/28.
+//
+
+#ifndef GPUIMAGE_X_XTRANSFORM_HPP
+#define GPUIMAGE_X_XTRANSFORM_HPP
+
+#include "XMacros.hpp"
+
+NS_X_IMAGE_BEGIN
+class XTransform {
+
+};
+NS_X_IMAGE_END
+
+#endif //GPUIMAGE_X_XTRANSFORM_HPP
